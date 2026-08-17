41a740f5f1c057ef8dafa696d2d4be2c13f4a0fd
Antony <63504047+pallupz@users.noreply.github.com>
2026-08-17 15:52:59 +1000

    chore: change application id to com.pallupz.aabrowser

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     compileSdk = 37
 
     defaultConfig {
-        applicationId = "com.kododake.aabrowser"
+        applicationId = "com.pallupz.aabrowser"
         minSdk = 35
         targetSdk = 37
         versionCode = 8
